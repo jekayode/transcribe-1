ff58dce6b871313f24154b90efba4677c3b2885a
Mbah Clinton <clintonmbah44@gmail.com>
2018-04-23 15:15:26 +0000

    started filling out base project

--- src/js/appController.js
+++ src/js/appController.js
@@ -6,12 +6,36 @@ define([
 ], function(oj, ko) {
 	function ControllerViewModel() {
 		var self = this;
-		self.filename = ko.observable('sample.mp3');
+		self.filename = ko.observable('No file selected');
+		self.googleText = ko.observable('');
+		self.amazonText = ko.observable('');
+		self.transcriptionProgress = ko.observable('');
+		self.supportedFiles = ['audio/wav', 'audio/mp3'];
 
 		self.selectListener = function(event) {
 			var files = event.detail.files;
-			for (var i = 0; i < files.length; i++) {
-				self.fileNames.push(files[i].name);
+
+			if (self.supportedFiles.indexOf(files[0].type) >= 0) {
+				self.filename(files[0].name);
+				self.transcriptionProgress('Transcribing file');
+
+				var reader = new FileReader();
+				reader.readAsDataURL(files[0]);
+				reader.onload = function() {
+					axios
+						.post('https://rightful-blowgun.glitch.me/transcribe/google', {
+							audio: result
+						})
+						.then(function(response) {
+							self.googleText(response.data.transcription);
+							self.transcriptionProgress('Transcription completed');
+						});
+				};
+				reader.onerror = function(error) {
+					alert('Error transcribing file: ', error);
+				};
+			} else {
+				alert('Invalid file selected. Please select an MP3');
 			}
 		};
 	}
