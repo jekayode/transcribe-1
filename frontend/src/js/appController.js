923ff7c81754e20337ffcdaa44e51ab003a1149c
Mbah Clinton <clintonmbah44@gmail.com>
2018-04-24 17:13:38 +0000

    added regex to validate that the right file is being chosen

--- frontend/src/js/appController.js
+++ frontend/src/js/appController.js
@@ -32,45 +32,61 @@ define([
 				self.amazonText(response.data.transcription);
 				self.amazonTranscriptionProgress('Amazon transcription completed.');
 			} else {
-				self.amazonTranscriptionProgress(
-					'Please wait. Transcription job not yet completed.'
-				);
+				alert('Please wait. Transcription job not yet completed.');
 			}
 		};
 
 		self.selectListener = function(event) {
 			var files = event.detail.files;
 
-			self.filename(files[0].name);
-			self.amazonTranscriptionProgress('Transcribing file');
-			self.googleTranscriptionProgress('');
-			self.amazonText('');
-			self.googleText('');
+			if (files[0].name.match(/\.wav$|\.flac$|\.mp3/g)) {
+				self.filename(files[0].name);
+				self.amazonTranscriptionProgress('Transcribing file');
+				self.googleTranscriptionProgress('');
+				self.amazonText('');
+				self.googleText('');
 
-			let data = new FormData();
-			data.append('audio', files[0]);
+				let extension = files[0].name.substring(
+					files[0].name.lastIndexOf('.'),
+					files[0].name.length
+				);
+				console.log(extension);
 
-			axios
-				.post('https://rightful-blowgun.glitch.me/transcribe/google', data)
-				.then(response => {
-					self.googleText(response.data.transcription);
-					self.googleTranscriptionProgress('Google transcription completed.');
-				})
-				.catch(error => {
-					console.log(error);
-					self.googleTranscriptionProgress('Google transcription failed.');
-				});
+				let data = new FormData();
+				data.append('audio', files[0]);
 
-			axios
-				.post('https://rightful-blowgun.glitch.me/transcribe/amazon', data)
-				.then(response => {
-					self.amazonJobName = response.data.transcriptionJobName;
-					self.amazonTranscriptionProgress('Amazon transcription job started.');
-				})
-				.catch(error => {
-					console.log(error);
-					self.amazonTranscriptionProgress('Amazon transcription failed.');
-				});
+				axios
+					.post(
+						`https://rightful-blowgun.glitch.me/transcribe/google/${extension}`,
+						data
+					)
+					.then(response => {
+						self.googleText(response.data.transcription);
+						self.googleTranscriptionProgress('Google transcription completed.');
+					})
+					.catch(error => {
+						console.log(error);
+						self.googleTranscriptionProgress('Google transcription failed.');
+					});
+
+				axios
+					.post(
+						`https://rightful-blowgun.glitch.me/transcribe/amazon/${extension}`,
+						data
+					)
+					.then(response => {
+						self.amazonJobName = response.data.transcriptionJobName;
+						self.amazonTranscriptionProgress(
+							'Amazon transcription job started.'
+						);
+					})
+					.catch(error => {
+						console.log(error);
+						self.amazonTranscriptionProgress('Amazon transcription failed.');
+					});
+			} else {
+				alert('Please select a valid MP3/WAV/FLAC file.');
+			}
 		};
 	}
 
